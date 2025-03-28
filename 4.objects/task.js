b21495192cf953eb55bf43049c824e825019ef86
User <user@MacBook-Pro-User.local>
2025-03-28 17:31:37 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,50 @@
 function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    }
+    
+    const student1 = new Student ("Иван", "Мужской", 20);
+    const student2 = new Student ("Ольга", "женский", 21);
+    
+    Student.prototype.setSubject = function (subjectName) {
+      this.subject = subjectName;
+    }
+    student1.setSubject("Физика");
+    student1.setSubject("История");
+    
+    Student.prototype.addMarks = function (...marks) {
+      if (!this.hasOwnProperty('marks')) {
+        console.error(`${this.name} Студент отчислен! Нет оценок!`);
+        return;
+      }
+      this.marks.push(...marks);
+    }
+    student1.addMarks(3, 4, 3, 5);
+    student2.addMarks(4, 4, 5, 4);
+    
+    Student.prototype.getAverage = function () {
+       if (!this.marks || this.marks.length === 0){
+        return 0;
+       }
+       let avg = this.marks.reduce(function(accumulate, currentValue){
+        return accumulate + currentValue;
+       }, 0)
+       return avg / this.marks.length;
+      }
+    
+    
+    
+    Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.marks;
+    delete this.subject;
+    }
+    student1.exclude('Отчислен');
+    
+    class Student {
+      constructor(){
+        
+      }
+    }
