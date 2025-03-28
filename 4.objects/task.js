cef4bfd1aa965227b8fc86463d50aa37eef3ad79
User <user@MacBook-Pro-User.local>
2025-03-28 17:44:33 +0300

    исправлены замечания3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -34,3 +34,15 @@ Student.prototype.exclude = function (reason) {
     this.marks = undefined; 
     this.excluded = reason; 
 };
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); 
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
