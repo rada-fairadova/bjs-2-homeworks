1b51612f1b7a822d336baca0970ba314c03016f3
User <user@MacBook-Pro-User.local>
2025-03-28 17:37:14 +0300

    исправление замечаний

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,46 +5,36 @@ function Student(name, gender, age) {
     this.marks = [];
     }
     
-    const student1 = new Student ("Иван", "Мужской", 20);
-    const student2 = new Student ("Ольга", "женский", 21);
+    Student.prototype.setSubject = function(subjectName) {
+        this.subject = subjectName;
+    };
     
-    Student.prototype.setSubject = function (subjectName) {
-      this.subject = subjectName;
-    }
-    student1.setSubject("Физика");
-    student1.setSubject("История");
+    Student.prototype.addMarks = function(...marksToAdd) {
+        if (!this.marks) return; 
+        const validMarks = marksToAdd.filter(mark => mark >= 1 && mark <= 5); 
+        this.marks.push(...validMarks); 
+    };
     
-    Student.prototype.addMarks = function (...marks) {
-      if (!this.hasOwnProperty('marks')) {
-        console.error(`${this.name} Студент отчислен! Нет оценок!`);
-        return;
-      }
-      this.marks.push(...marks);
-    }
-    student1.addMarks(3, 4, 3, 5);
-    student2.addMarks(4, 4, 5, 4);
+    Student.prototype.getAverage = function() {
+        if (!this.marks || this.marks.length === 0) return 0; 
+        const total = this.marks.reduce((sum, mark) => sum + mark, 0); 
+        return total / this.marks.length; 
+    };
     
-    Student.prototype.getAverage = function () {
-       if (!this.marks || this.marks.length === 0){
-        return 0;
-       }
-       let avg = this.marks.reduce(function(accumulate, currentValue){
-        return accumulate + currentValue;
-       }, 0)
-       return avg / this.marks.length;
-      }
+    Student.prototype.exclude = function(reason) {
+        delete this.subject; 
+        delete this.marks; 
+        this.excluded = reason; 
+    };
     
+    Student.prototype.getInfo = function() {
+        return `${this.name}, ${this.gender}, ${this.age} years old`;
+    };
     
+    Student.prototype.getMarksCount = function() {
+        return this.marks.length;
+    };
     
-    Student.prototype.exclude = function (reason) {
-    this.excluded = reason;
-    delete this.marks;
-    delete this.subject;
-    }
-    student1.exclude('Отчислен');
-    
-    class Student {
-      constructor(){
-        
-      }
-    }
+    Student.prototype.getBestMark = function() {
+        return Math.max(...this.marks);
+    };
