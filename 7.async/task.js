2da7afa3c0db4d6cc005c5d040336df93749c6f6
User <user@MacBook-Pro-User.local>
2025-04-14 20:57:25 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,58 @@
+class AlarmClock {
+    constructor(intervalId){
+     this.alarmCollection = [];
+     this.intervalId = null;
+    }
+  
+    addClock(time, callback) {
+     if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+     }
+     if(this.alarmCollection.some(alarm => alarm.time === time)){
+      console.warn('Уже присутствует звонок на это же время');
+     }
+     this.alarmCollection.push ({
+      callback,
+      time,
+      canCall: true
+     });
+    }
+    removeClock(time){
+      this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+    getCurrentFormattedTime (){
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      return  `${hours}:${minutes}`;
+    }
+    start(){
+      if(this.intervalId !== null){
+         return;
+      }
+      this.intervalId = setInterval(() => {
+        const currenTime = this.getCurrentFormattedTime();
+        this.alarmCollection.forEach(alarm => {
+          if (alarm.time === currenTime && alarm.canCall) {
+            alarm.canCall = false;
+            alarm.callback();
+          }
+        });
+      }, 1000);
+    }
+    stop(){
+     if (this.intervalId){
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+     }
+    }
+    resetAllCalls(){
+      this.alarmCollection.forEach(alarm => {
+        alarm.canCall =true
+        });
+    }
+    clearAlarms(){
+      this.stop();
+      this.alarmCollection = [];
+    }
+  }
